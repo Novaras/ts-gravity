69c860d715104d2ce08ea798244668c1e14ea261
Novaras <tfear091@gmail.com>
2022-08-22 15:53:52 +0100

    mega optimisation for physics loop

--- ts-gravity/src/phase/physics.ts
+++ ts-gravity/src/phase/physics.ts
@@ -2,7 +2,7 @@ import KineticObj from "../KineticObj";
 import { gravitateBoth } from "../PhysicsLib";
 
 export default (kinetic_objs: KineticObj[]) => {
-	for (let i = 0; i < kinetic_objs.length - 1; ++i) {
+	for (let i = 0; i < Math.ceil(Math.sqrt(kinetic_objs.length - 1)); ++i) {
 		const k1 = kinetic_objs[i];
 		for (let j = i + 1; j < kinetic_objs.length; ++j) {
 			const k2 = kinetic_objs[j];
